96e313829b6f3620668c578ff6ee5c8d66d97ec2
nicktrem <nicholas.tremaroli@gmail.com>
2025-05-03 17:30:13 -0400

    Added some tests to the fund controller

--- test/Fund.ts
+++ test/Fund.ts
@@ -6,36 +6,52 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import hre from "hardhat";
 
+
+const oneDay = 24n * 60n * 60n;
 describe("Fund Functionalities", function ()
 {
+    async function contractDeploymentFixture()
+    {
+        const [owner] = await hre.ethers.getSigners();
+
+        // const mockAggregator = await hre.ethers.deployContract("MockV3Aggregator", [6, 1000000]);
+        // await mockAggregator.waitForDeployment();
+
+        const usdcMockTotalSupply = 1000000000n;
+        const usdcMockDecimals = 6n
+        const usdcMock = await hre.ethers.deployContract("GenericERC20Mock",
+                    ["USDC Mock", "USDM", usdcMockDecimals, usdcMockTotalSupply]);
+        await usdcMock.waitForDeployment();
+
+        // check the initial supply of the mock usdc
+        expect(await usdcMock.totalSupply()).to.equal(usdcMockTotalSupply * 10n ** usdcMockDecimals);
+
+        const initialEpochTime = oneDay;
+        const initialPercentageFeeProposers = 1;
+        const initialPercentageFeeGovernors = 1;
+        const fundController = await hre.ethers.deployContract("FundController",
+               [initialEpochTime, initialPercentageFeeProposers, initialPercentageFeeGovernors, await usdcMock.getAddress()]);
+
+        await fundController.waitForDeployment();
+
+        expect(await fundController.s_epochTime()).to.equal(initialEpochTime);
+        expect(await fundController.s_proposalPercentageReward()).to.equal(initialPercentageFeeProposers);
+        expect(await fundController.s_governorPercentrageReward()).to.equal(initialPercentageFeeGovernors);
+
+        const fundToken = await hre.ethers.deployContract("FundToken",[await fundController.getAddress()]);
+        await fundToken.waitForDeployment();
+
+        fundController.initialize(await fundToken.getAddress());
+
+        return { owner, usdcMock, fundToken, fundController }
+        
+    }
+
     describe("Initialization", function ()
     {
         it("Should deploy the contracts correctly", async function ()
         {
-
-            const [owner] = await hre.ethers.getSigners();
-
-            const usdcMockTotalSupply = 1000000000n;
-            const usdcMockDecimals = 6n
-            const usdcMock = await hre.ethers.deployContract("GenericERC20Mock",
-                        ["USDC Mock", "USDM", usdcMockDecimals, usdcMockTotalSupply]);
-            await usdcMock.waitForDeployment();
-
-            const oneDay = 24 * 60 * 60;
-            const percentageFeeProposers = 1;
-            const percentageFeeGovernors = 1;
-            const fundController = await hre.ethers.deployContract("FundController",
-                   [oneDay, percentageFeeProposers, percentageFeeGovernors, await usdcMock.getAddress()]);
-
-            await fundController.waitForDeployment();
-
-            const fundToken = await hre.ethers.deployContract("FundToken",[await fundController.getAddress()]);
-            await fundToken.waitForDeployment();
-
-            fundController.initialize(await fundToken.getAddress());
-
-            // check the initial supply of the mock usdc
-            expect(await usdcMock.totalSupply()).to.equal(usdcMockTotalSupply * 10n ** usdcMockDecimals);
+            const { owner, fundToken, fundController } = await loadFixture(contractDeploymentFixture);
 
             // check the ownership of the fund token
             expect(await fundToken.owner()).to.equal(await fundController.getAddress());
@@ -43,5 +59,39 @@ describe("Fund Functionalities", function ()
             // check the ownership of the fund fundController
             expect(await fundController.owner()).to.equal(await owner.getAddress());
          })
+    })
+
+    describe("Fund Controller", function ()
+    {
+        it("Should set the setter fields correctly", async function ()
+        {
+            const { fundController } = await loadFixture(contractDeploymentFixture);
+
+            // set the new epoch time to two days
+            const newEpochTime = 2n * oneDay;
+            await fundController.setEpochTime(newEpochTime);
+            // now check the new epoch time
+            expect(await fundController.s_epochTime()).to.equal(newEpochTime);
+
+            // set the new percentage fee for the proposers to 2%
+            const newPercentageFeeProposers = 2;
+            await fundController.setProposalPercentageReward(newPercentageFeeProposers);
+            // check the new percentage fee for the proposers
+            expect(await fundController.s_proposalPercentageReward()).to.equal(newPercentageFeeProposers);
+
+            // set the new percentage fee for the governors to 2%
+            const newPercentageFeeGovernors = 3;
+            await fundController.setGovernorPercentageReward(newPercentageFeeGovernors);
+            // check the new percentage fee for the governors
+            expect(await fundController.s_governorPercentrageReward()).to.equal(newPercentageFeeGovernors);
+
+        })
+    })
+    describe("Fund Token", function ()
+    {
+        it("Should add an asset to the fund token", async function ()
+        {
+            
+        })
     })
 });

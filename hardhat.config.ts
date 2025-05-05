22038a5ac8322ef5d94f2be2ac30b0ca093ae42d
nicktrem <nicholas.tremaroli@gmail.com>
2025-05-05 15:25:37 -0400

    Added config to have ERC20 transfers with zero gas
    NOTE: This is a temporary change just to make testing easier

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,11 +6,28 @@ const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks:
   {
-    hardhat: {},
+    hardhat:
+    {
+        // Should be changed when doing real testing
+        // but for now assume gas prices are 0
+        gasPrice: 0,
+        initialBaseFeePerGas: 0,
+    },
     base_mainnet:
     {
+      // Should be changed when doing real testing
+      // but for now assume gas prices are 0
+      gasPrice: 0,
+      initialBaseFeePerGas: 0,
       url: `${process.env.ALCHEMY_URL}/${process.env.ALCHEMY_API_KEY}`,
       accounts: [`${process.env.BASE_MAINNET_PRIVATE_KEY}`],
+    },
+    localhost:
+    {
+        // Should be changed when doing real testing
+        // but for now assume gas prices are 0
+        gasPrice: 0,
+        initialBaseFeePerGas: 0,
     }
   }
 };

ce92a90a3043424c2ab26178dde841b381ede82a
nicktrem <nicholas.tremaroli@gmail.com>
2025-05-05 14:44:28 -0400

    Added other base mainnet config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks:
+  {
+    hardhat: {},
+    base_mainnet:
+    {
+      url: `${process.env.ALCHEMY_URL}/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`${process.env.BASE_MAINNET_PRIVATE_KEY}`],
+    }
+  }
 };
 
 export default config;
